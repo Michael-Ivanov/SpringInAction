f9ff4ca2291222878988d6a52228f8d8d6591751
Michael Ivanov <eesseth@gmail.com>
2021-03-16 19:31:23 +0300

    Init commit: design controller ang view

---
+++ src/main/java/miv/study/tacos/Taco.java
@@ -0,0 +1,24 @@
+package miv.study.tacos;
+
+import java.util.List;
+
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
---
+++ src/main/java/miv/study/tacos/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package miv.study.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
