3587e288edbe94ee4822da6b3f569be9ed2f086f
Michael Ivanov <eesseth@gmail.com>
2021-03-16 22:17:16 +0300

    Add Order.class, order controller, order form view

---
+++ src/main/java/miv/study/tacos/web/OrderController.java
@@ -0,0 +1,29 @@
+package miv.study.tacos.web;
+
+import miv.study.tacos.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        logger.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
