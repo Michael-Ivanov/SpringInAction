ec89645d5e570a6291f6e899fdee5ec970070e6c
Michael Ivanov <eesseth@gmail.com>
2021-03-18 22:53:46 +0300

    Change design controller to work with jdbc, add jdbc properties

--- src/main/java/miv/study/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/miv/study/tacos/data/JdbcIngredientRepository.java
@@ -27,21 +27,21 @@ public class JdbcIngredientRepository implements IngredientRepository {
     public List<Ingredient> findAll() {
 
         return jdbcTemplate.query(
-                "select id, name, type, from Ingredient",
+                "select id, name, type from ingredient;",
                 this::mapRowToIngredient);
     }
 
     @Override
     public Ingredient findOne(String id) {
         return jdbcTemplate.queryForObject(
-                "select id, name, type from Ingredient where id=?",
+                "select id, name, type from ingredient where id=?",
                 this::mapRowToIngredient, id);
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
         jdbcTemplate.update(
-                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                "insert into ingredient (id, name, type) values (?, ?, ?)",
                 ingredient.getId(),
                 ingredient.getName(),
                 ingredient.getType().toString());
--- src/main/java/miv/study/tacos/web/DesignTacoController.java
+++ src/main/java/miv/study/tacos/web/DesignTacoController.java
@@ -4,8 +4,10 @@ package miv.study.tacos.web;
 import miv.study.tacos.Ingredient;
 import miv.study.tacos.Ingredient.Type;
 import miv.study.tacos.Taco;
+import miv.study.tacos.data.IngredientRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,22 +28,18 @@ public class DesignTacoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DesignTacoController.class);
 
+    private final IngredientRepository ingredientRepository;
 
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        ingredientRepository.findAll().forEach(ingredients::add);
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
