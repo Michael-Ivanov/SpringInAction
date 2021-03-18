0c5ba7e49effb41c32232f9abf381f8263b88e4a
Michael Ivanov <eesseth@gmail.com>
2021-03-18 19:51:42 +0300

    Replace HomeController class with addViewControllers(..) method

--- src/main/java/miv/study/tacos/web/DesignTacoController.java
+++ src/main/java/miv/study/tacos/web/DesignTacoController.java
@@ -2,25 +2,22 @@ package miv.study.tacos.web;
 
 
 import miv.study.tacos.Ingredient;
+import miv.study.tacos.Ingredient.Type;
 import miv.study.tacos.Taco;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import miv.study.tacos.Ingredient.Type;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/design")
