7d86506ba3fa6f0077689dc818961527a5d2893a
Michael Ivanov <eesseth@gmail.com>
2021-03-18 23:17:43 +0300

    Add Order and Taco repository interfaces

---
+++ src/main/java/miv/study/tacos/data/OrderRepository.java
@@ -0,0 +1,9 @@
+package miv.study.tacos.data;
+
+import miv.study.tacos.Order;
+
+public interface OrderRepository {
+
+    Order save(Order order);
+
+}
---
+++ src/main/java/miv/study/tacos/data/TacoRepository.java
@@ -0,0 +1,9 @@
+package miv.study.tacos.data;
+
+import miv.study.tacos.Taco;
+
+public interface TacoRepository {
+
+    Taco save(Taco design);
+
+}
