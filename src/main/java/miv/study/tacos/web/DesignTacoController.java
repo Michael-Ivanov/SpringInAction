21c17e1236de3a8d1e9542711cc7f4a52a3f6802
Michael Ivanov <eesseth@gmail.com>
2021-03-20 22:32:35 +0300

    Rafactor: design taco controller to process order

--- src/main/java/miv/study/tacos/web/DesignTacoController.java
+++ src/main/java/miv/study/tacos/web/DesignTacoController.java
@@ -3,36 +3,37 @@ package miv.study.tacos.web;
 
 import miv.study.tacos.Ingredient;
 import miv.study.tacos.Ingredient.Type;
+import miv.study.tacos.Order;
 import miv.study.tacos.Taco;
 import miv.study.tacos.data.IngredientRepository;
+import miv.study.tacos.data.TacoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DesignTacoController.class);
 
     private final IngredientRepository ingredientRepository;
+    private final TacoRepository tacoRepository;
 
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepository) {
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.tacoRepository = tacoRepository;
     }
 
     @ModelAttribute
@@ -47,23 +48,33 @@ public class DesignTacoController {
         }
     }
 
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
     @GetMapping
     public String showDesignForm(Model model) {
 
-        model.addAttribute("design", new Taco());
         return "design";
     }
 
     @PostMapping
-    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+    public String processDesign(@Valid Taco taco, Errors errors, @ModelAttribute Order order) {
 
         if (errors.hasErrors()) {
-            logger.warn("Taco object not valid: " + design);
+            logger.warn("Taco object not valid: " + taco);
             return "/design";
         }
 
-        // Save the taco design...
-        logger.info("Processing design: " + design);
+        Taco savedTaco = tacoRepository.save(taco);
+        order.addTaco(savedTaco);
+        logger.info("Processing design: " + taco);
         return "redirect:/orders/current";
     }
 
