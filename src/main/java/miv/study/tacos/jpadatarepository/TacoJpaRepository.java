7b21f7d2032f3ebf6f436ad58939bc782201be24
Michael Ivanov <eesseth@gmail.com>
2021-03-28 20:27:57 +0300

    Add rest controller handling /design/recent and /design/{id} endpoint requests

--- src/main/java/miv/study/tacos/jpadatarepository/TacoJpaRepository.java
+++ src/main/java/miv/study/tacos/jpadatarepository/TacoJpaRepository.java
@@ -1,7 +1,7 @@
 package miv.study.tacos.jpadatarepository;
 
 import miv.study.tacos.Taco;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoJpaRepository extends CrudRepository<Taco, Long> {
+public interface TacoJpaRepository extends PagingAndSortingRepository<Taco, Long> {
 }
---
+++ src/main/java/miv/study/tacos/web/api/DesignTacoRestController.java
@@ -0,0 +1,41 @@
+package miv.study.tacos.web.api;
+
+import miv.study.tacos.Taco;
+import miv.study.tacos.jpadatarepository.TacoJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoRestController {
+
+    private TacoJpaRepository tacoJpaRepository;
+
+    @Autowired
+    public DesignTacoRestController(TacoJpaRepository tacoJpaRepository) {
+        this.tacoJpaRepository = tacoJpaRepository;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest pageRequest = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return tacoJpaRepository.findAll(pageRequest).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optionalTaco = tacoJpaRepository.findById(id);
+        if (optionalTaco.isPresent()) {
+            return new ResponseEntity<>(optionalTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+}
