6ac6bf6c5b90cfc01b730ad51a615dd37cec6295
Michael Ivanov <eesseth@gmail.com>
2021-04-04 20:38:55 +0300

    Change: add Lombok support to Taco class, add explicit @RestResource relation and path to Taco class

--- src/main/java/miv/study/tacos/Taco.java
+++ src/main/java/miv/study/tacos/Taco.java
@@ -1,5 +1,8 @@
 package miv.study.tacos;
 
+import lombok.Data;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -7,7 +10,9 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
+@Data
 @Entity
+@RestResource(rel = "tacos", path = "tacos")
 public class Taco {
 
     @Id
@@ -29,53 +34,8 @@ public class Taco {
     @NotEmpty(message = "You must choose at lease one ingredient")
     private List<Ingredient> ingredients;
 
-    public Taco() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @PrePersist
     private void createdAt() {
         this.createdAt = new Date();
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    @Override
-    public String toString() {
-        return "Taco{" +
-                "id=" + id +
-                ", createdAt=" + createdAt +
-                ", name='" + name + '\'' +
-                ", ingredients=" + ingredients +
-                '}';
     }
 }
