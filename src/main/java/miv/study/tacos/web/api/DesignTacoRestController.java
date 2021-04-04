8cd4c923f1272ffc8295fe286993432e321d496e
Michael Ivanov <eesseth@gmail.com>
2021-04-04 19:09:24 +0300

    Add hateoas responce support for 'recent' controller

--- src/main/java/miv/study/tacos/web/api/DesignTacoRestController.java
+++ src/main/java/miv/study/tacos/web/api/DesignTacoRestController.java
@@ -3,12 +3,18 @@ package miv.study.tacos.web.api;
 import miv.study.tacos.Taco;
 import miv.study.tacos.jpadatarepository.TacoJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,10 +30,18 @@ public class DesignTacoRestController {
     }
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos() {
+    public CollectionModel<EntityModel<Taco>> recentTacos() {
         PageRequest pageRequest = PageRequest.of(
                 0, 12, Sort.by("createdAt").descending());
-        return tacoJpaRepository.findAll(pageRequest).getContent();
+
+        List<Taco> tacos = tacoJpaRepository.findAll(pageRequest).getContent();
+        CollectionModel<EntityModel<Taco>> recentResources =
+                CollectionModel.wrap(tacos);
+
+        recentResources.add(
+                new Link("http://localhost:8888/design/recent", "recents"));
+
+        return recentResources;
     }
 
     @GetMapping("/{id}")
