81b3db167892ddb05849d895ad94a6bfc3d6350f
Michael Ivanov <eesseth@gmail.com>
2021-04-06 22:14:58 +0300

    Disable csrf in SecurityConfig to allow updating DB by REST PUT requests

--- src/main/java/miv/study/tacos/security/SecurityConfig.java
+++ src/main/java/miv/study/tacos/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/design", "/orders").hasRole("USER")
                 .antMatchers("/", "/**").permitAll()
