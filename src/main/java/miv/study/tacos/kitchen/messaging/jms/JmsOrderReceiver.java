c6c5f945732f13aa6c3b5ae7ea7484f34353be94
Michael Ivanov <eesseth@gmail.com>
2021-04-08 22:32:52 +0300

    Change JmsTemplate receive method to receiveAndConver

--- src/main/java/miv/study/tacos/kitchen/messaging/jms/JmsOrderReceiver.java
+++ src/main/java/miv/study/tacos/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -3,32 +3,20 @@ package miv.study.tacos.kitchen.messaging.jms;
 import miv.study.tacos.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
 
 @Component
 public class JmsOrderReceiver implements OrderReceiver {
 
     private JmsTemplate jmsTemplate;
-    private MessageConverter converter;
 
     @Autowired
-    public JmsOrderReceiver(JmsTemplate jmsTemplate, MessageConverter converter) {
+    public JmsOrderReceiver(JmsTemplate jmsTemplate) {
         this.jmsTemplate = jmsTemplate;
-        this.converter = converter;
     }
 
     @Override
     public Order receiveOrder() {
-        Message message = jmsTemplate.receive("tacocloud.order.queue");
-        try {
-            return (Order) converter.fromMessage(message);
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }
-        throw new RuntimeException("something id wrong");
+        return (Order) jmsTemplate.receiveAndConvert("tacocloud.order.queue");
     }
 }
