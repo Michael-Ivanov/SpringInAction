5c02f187f0a51ee3ab9c59c6a911ec0d3bb861f8
Michael Ivanov <eesseth@gmail.com>
2021-04-08 20:32:45 +0300

    Add Bean definition - messageConverter

--- src/main/java/miv/study/tacos/TacoCloudApplication.java
+++ src/main/java/miv/study/tacos/TacoCloudApplication.java
@@ -4,10 +4,18 @@ import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConversionException;
+import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class TacoCloudApplication implements WebMvcConfigurer {
@@ -25,5 +33,18 @@ public class TacoCloudApplication implements WebMvcConfigurer {
     @Bean
     public Destination orderQueue() {
         return new ActiveMQQueue("tacocloud.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter =
+                new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("order", Order.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+
+        return messageConverter;
     }
 }
