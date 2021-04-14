1ba41e7ce86e137a69587ac033bb557a8d4b455b
Michael Ivanov <eesseth@gmail.com>
2021-04-14 21:37:59 +0300

    Add header to message, using anonimous MessagePostProcessor instance

--- src/main/java/miv/study/tacos/messaging/RabbitOrderMessagingService.java
+++ src/main/java/miv/study/tacos/messaging/RabbitOrderMessagingService.java
@@ -1,6 +1,10 @@
 package miv.study.tacos.messaging;
 
 import miv.study.tacos.Order;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,13 @@ public class RabbitOrderMessagingService implements OrderMessagingService{
 
     @Override
     public void sendOrder(Order order) {
-        rabbitTemplate.convertAndSend("hello world queue", order);
+        rabbitTemplate.convertAndSend("hello world queue", order, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                MessageProperties properties = message.getMessageProperties();
+                properties.setHeader("X_ORDER_SOURCE", "WEB");
+                return message;
+            }
+        });
     }
 }
